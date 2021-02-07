5efff09c6ebc8c5b287b7279f8a91c22e4a40ba5
cngzklc <45482531+cngzklc@users.noreply.github.com>
2021-02-08 02:04:33 +0300

    CategoryManager GetById test Update

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,17 +9,18 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductTest();
-            //CategoryTest();
+            //ProductTest();
+            CategoryTest();
         }
 
         private static void CategoryTest()
         {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
-            {
-                Console.WriteLine(category.CategoryName);
-            }
+            Console.WriteLine("ID = {0}",categoryManager.GetById(152).CategoryName);
+            //foreach (var category in categoryManager.GetAll())
+            //{
+            //    Console.WriteLine(category.CategoryName);
+            //}
         }
 
         private static void ProductTest()
